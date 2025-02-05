2c6bb82dc626bb4108b6b4cc93111535d0c4ece9
maamokun/MikanDev <maamo-kun@outlook.com>
2025-02-06 02:46:42 +0900

    feat: very basic thing for SES reception

--- packages/ses-reciever/src/index.ts
+++ packages/ses-reciever/src/index.ts
@@ -2,8 +2,9 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/', (c) => {
+  const body = c.req.text()
+  console.log(body)
 })
 
 export default app
