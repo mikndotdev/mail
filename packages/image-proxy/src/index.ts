1c988c26fea91f66d7258835c23c0261fbd2e5e4
maamokun/MikanDev <maamo-kun@outlook.com>
2025-02-06 01:58:30 +0900

    feat: basic Image proxy

--- packages/image-proxy/src/index.ts
+++ packages/image-proxy/src/index.ts
@@ -1,9 +1,26 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/:key/:url', async (c) => {
+  const key = c.req.param('key');
+  const url = c.req.param('url');
 
-export default app
+  if (!key || !url) {
+    return c.json({ error: 'Invalid request' }, 400);
+  }
+
+  const data = await fetch(url);
+
+  if (!data.ok) {
+    return c.json({ error: 'Failed to fetch image' }, 500);
+  }
+  const contentType = data.headers.get('content-type');
+  if (!contentType?.startsWith('image')) {
+    return c.json({ error: 'Invalid content type' }, 400);
+  }
+
+  return new Response(data.body, { headers: { 'content-type': contentType } });
+});
+
+export default app;
